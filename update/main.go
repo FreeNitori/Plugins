058034c504fcf6c9875949956a777bc700877350
RandomChars <random@chars.jp>
2021-02-15 18:53:28 +0900

    add plugins

---
+++ update/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"git.randomchars.net/FreeNitori/FreeNitori/nitori/multiplexer"
+	"git.randomchars.net/FreeNitori/FreeNitori/nitori/state"
+	"os/exec"
+)
+
+// CommandRoute contains information of a command route.
+//goland:noinspection GoUnusedGlobalVariable
+var CommandRoute = multiplexer.Route{
+	Pattern:       "update",
+	AliasPatterns: []string{""},
+	Description:   "",
+	Category:      multiplexer.SystemCategory,
+	Handler: func(context *multiplexer.Context) {
+		if !context.IsAdministrator() {
+			context.SendMessage(state.AdminOnly)
+			return
+		}
+
+		context.SendMessage("Compiling...")
+		command := exec.Command("/bin/sh", "-c", "make && make plugins")
+		var output bytes.Buffer
+		command.Stdout = &output
+		command.Stderr = &output
+		err := command.Run()
+		if err != nil {
+			context.SendMessage("Error occurred while compiling.")
+			context.SendMessage(fmt.Sprintf("```\n%s\n```", output.String()))
+			return
+		}
+		context.SendMessage("Finished compiling, attempting restart...")
+		state.ExitCode <- -1
+	},
+}
